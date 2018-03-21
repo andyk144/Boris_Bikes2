62ee557f436c857a4ab3c743728cfcc9bc79c967
Patryk Pilecki <ppilecki@icloud.com>
2018-03-21 13:14:44 +0000

    test - get a bike? - working?

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,10 @@
 require 'dockingstation'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike)  }
+  it 'returns instance of bike class when called release_bike' do
+    expect(DockingStation.new.release_bike).to be_a Bike
+  end
+  it 'expect bike to working? or not' do
+    expect(Bike.new.working?).to eq true
+  end
 end
